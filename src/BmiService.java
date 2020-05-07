ee781089ebd0280075a6492f524aa4ae84d52279
Mikhail19870105 <crazyhtc87@gmail.com>
2020-05-07 17:05:24 +0300

    Modified BMI

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double height, double weight) {
-        double bmIndex = (int) (weight / (height * height / 100 / 100));
+        double bmIndex = (int) (weight / Math.pow(height / 100, 2));
         return (bmIndex);
     }
 
